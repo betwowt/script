de99f1ddfd5aed12565b817bdbf4ffde4cc1c2cf
zhengf.l <zfliu@nanshudata.com>
2020-08-12 15:52:51 +0800

    提交nginx 安装脚本

---
+++ web/install_nginx.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yum update -y
+yum install yum-utils -y
+
+(
+cat << EOF
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+EOF
+) > /etc/yum.repos.d/nginx.repo
+
+yum-config-manager --enable nginx-mainline
+yum install nginx -y
+
